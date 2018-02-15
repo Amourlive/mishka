798f68500bca9e8d9e02dc3ce96403297d12529a
Amourlive <aleksei_mackevich@outlook.com>
2018-02-15 22:25:39 +0300

    Fix svg sprite script

--- tasks/styles_svg.js
+++ tasks/styles_svg.js
@@ -1,15 +1,43 @@
 'use strict';
 
-const gulp = require("gulp");
-const rename = require("gulp-rename");
-const svgStore = require('gulp-svgstore');
+const gulp = require("gulp"),
+    svgSprite = require('gulp-svg-sprite'),
+    svgmin = require('gulp-svgmin'),
+    cheerio = require('gulp-cheerio'),
+    replace = require('gulp-replace');
 
 module.exports = function (options) {
     return function () {
         return gulp.src(options.src)
-            .pipe(svgStore({inlineSvg: true
-    }))
-            .pipe(rename("sprite.svg"))
-            .pipe(gulp.dest("public/styles"));
+            .pipe(svgmin({
+                js2svg: {
+                    pretty: true
+                }
+            }))
+            // remove all fill, style and stroke declarations in out shapes
+            .pipe(cheerio({
+                run: function ($) {
+                    $('[fill]').removeAttr('fill');
+                    $('[stroke]').removeAttr('stroke');
+                    $('[style]').removeAttr('style');
+                },
+                parserOptions: {xmlMode: true}
+            }))
+            // cheerio plugin create unnecessary string '&gt;', so replace it.
+            .pipe(replace('&gt;', '>'))
+            // build svg sprite
+            .pipe(svgSprite({
+                mode: {
+                    symbol: {
+                        sprite: "../sprite.svg",
+                        render: {
+                            css: {
+                                dest:'sprite.css'
+                            }
+                        }
+                    }
+                }
+            }))
+            .pipe(gulp.dest('public/styles'));
     };
 };
